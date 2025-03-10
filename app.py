a0a178c5c3f88be5a9c0baea8e6afc79b821fefe
barkha <107093216+my-projects-it@users.noreply.github.com>
2025-03-10 15:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def recommend():
     return jsonify({"status": "trained"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # ✅ Clever Cloud ke liye sahi Flask server setup
+    app.run(host='0.0.0.0', port=8080)
 
 
 # In[ ]:
